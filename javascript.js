7fb6ddde0a72166379bdd151d15a9ad6d1f72f96
Beelcode <belacode174@gmail.com>
2023-12-20 00:58:20 +0800

    w3s

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function klik() {
+    //    document.getElementById("klik1").innerHTML = 5 + 6;
+
+    //    document.write(8 + 9);
+    //window.alert("8 + 9");
+    //    alert(8 + 9);
+    console.log("coabaaaaaaaaaaaa");
+}
